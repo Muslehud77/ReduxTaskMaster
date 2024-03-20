a7cfeb0e35fa5cc37f81337410e15a9ba489913b
Muslehud77 <fardinmohit@gmail.com>
2024-03-21 02:54:34 +0800

    added reuseable modal

---
+++ src/components/tasks/AddTaskModal.jsx
@@ -0,0 +1,18 @@
+import Modal from "../Ui/Modal";
+
+
+const AddTaskModal = ({isOpen,setIsOpen}) => {
+    return (
+        <Modal isOpen={isOpen} setIsOpen={setIsOpen} title="Add Task">
+            
+            <p>Hello</p>
+            
+            
+            
+            </Modal>
+           
+       
+    );
+};
+
+export default AddTaskModal;
