084a891c2a25bc74da11cb8b3bfbe5414cff20b4
Muslehud77 <fardinmohit@gmail.com>
2024-03-23 01:49:59 +0800

    added unique id to  addtask reducer

--- src/Redux/features/tasks/tasksSlice.js
+++ src/Redux/features/tasks/tasksSlice.js
@@ -9,8 +9,16 @@ const tasksSlice = createSlice({
     name: 'tasks',
     initialState,
     reducers: {
-      addTask : (actions,{payload})=>{
-        actions.tasks.push(payload)
+      addTask : (state,{payload})=>{
+
+        if(state.tasks.length ===0){
+            
+            state.tasks.push({id:1,...payload})
+        }else{
+           const lastElement = state.tasks.at(-1)
+            state.tasks.push({ id: lastElement.id+1 , ...payload });
+        }
+
       }
     },
 })
